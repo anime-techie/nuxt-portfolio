dcbb4206083ae55078653595f1bf4d57b32e8026
Rakesh Yadav <55130299+rakeshyadav-tech@users.noreply.github.com>
2020-10-29 11:23:25 +0530

    installed gsap with an example

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,7 @@ export default {
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
+    'nuxt-gsap' ,
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <Header/>
+     <h1 ref="test" class="test">
+    Works!
+  </h1>
   </div>
 </template>
 
@@ -9,10 +12,20 @@ import Header from '~/components/Header';
 export default {
   components:{
     Header
+  },
+  mounted () {
+    this.$nextTick(() => { // When using $refs, must wait for nextTick
+      const tl = this.$gsap.timeline({ repeat: -1, ease: this.$gsap.Linear.easeInOut(2) })
+      tl.to('.test', 2, { x: 200 }) // With css selector
+      tl.to(this.$refs.test, 0.5, { x: 0 }) // With refs
+    })
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+.test {
+    color: red;
+    display: inline-block;
+  }
 </style>
